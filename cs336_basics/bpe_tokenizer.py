c92d15a74368c984ea6c35b023bbb123c1c685e4
Abhinav Aggarwal <abhinavaggrwal@gmail.com>
2026-04-20 22:19:51 +0200

    BPE parallel processing

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -1,9 +1,11 @@
 from collections import defaultdict
 import regex as re
 from copy import deepcopy
+from multiprocessing import Pool
+import json
+from tqdm import tqdm
 
-
-# from cs336_basics.pretokenization_example import find_chunk_boundaries
+from cs336_basics.pretokenization_example import find_chunk_boundaries
 
 class BPETokenizer:
     def __init__(self, vocab_size, special_tokens=["<|endoftext|>"]):
@@ -30,21 +32,48 @@ class BPETokenizer:
         return self._vocab
     
     def fit(self, input_path: str):
-        with open(input_path, "r") as f:
-            data = f.read()
+        with open(input_path, "rb") as f:
+            num_processes = 8
+            boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
-        for chunk in re.split(self._delimiter, data):
+            process_chunks = []
+            for start, end in zip(boundaries[:-1], boundaries[1:]):
+                f.seek(start)
+                chunk = f.read(end - start).decode("utf-8", errors="ignore")
+                process_chunks.append(chunk)
+
+        with Pool(processes=num_processes) as pool:
+            results = pool.map(self._pre_tokenize, process_chunks)
+        
+        self._combine_pool_results(results)
+
+        num_merges = self._vocab_size - len(self._vocab)
+        with tqdm(total=num_merges, desc="BPE Merges", leave=True) as pbar:
+            while len(self._vocab) < self._vocab_size:
+                to_merge_pair = self._find_merge_pair()
+                self._merge_pair(to_merge_pair)
+                pbar.update(1)
+
+    def _pre_tokenize(self, process_chunk):
+        counts = defaultdict(int)
+        pairwise_counts = defaultdict(int)
+        for chunk in tqdm(re.split(self._delimiter, process_chunk), desc="Pre-tokenizing"):
             for token in re.finditer(self._pat, chunk):
                 encoded_token = token.group().encode("utf-8")
                 byte_list = tuple([bytes([elem]) for elem in encoded_token])
-                self._counts[byte_list] += 1
+                counts[byte_list] += 1
                 for i in range(len(byte_list)-1):
-                    self._pairwise_count[(byte_list[i], byte_list[i+1])] += 1
-
-        while len(self._vocab) < self._vocab_size:     
-            to_merge_pair = self._find_merge_pair()
-            self._merge_pair(to_merge_pair)
-
+                    pairwise_counts[(byte_list[i], byte_list[i+1])] += 1
+        return counts, pairwise_counts
+    
+    def _combine_pool_results(self, pool_results):
+        for counts, pairwise_counts in pool_results:
+            for pre_token, count in counts.items():
+                self._counts[pre_token] += count
+            
+            for pair, pairwise_count in pairwise_counts.items():
+                self._pairwise_count[pair] += pairwise_count
+ 
     def _find_merge_pair(self):
         sorted_pairs = sorted(self._pairwise_count.items(), key=lambda x: x[1], reverse=True)
         most_common_pair = sorted_pairs[0]
@@ -63,7 +92,6 @@ class BPETokenizer:
         self._vocab[vocab_size] = merged_pair
         self._pairwise_count.pop(pair)
 
-        # pre_token_counts = deepcopy(self._counts)
         to_update = []
         for pre_token, count in self._counts.items():
 
@@ -108,23 +136,32 @@ class BPETokenizer:
             self._counts[new] += self._counts.pop(old)
 
     def _update_pre_token(self, pre_token, match_locations):
-        updated_token = deepcopy(pre_token)
         for idx, location in enumerate(match_locations):
             match_location = location - idx
-            updated_token = updated_token[:match_location] + (updated_token[match_location] + updated_token[match_location+1],) + updated_token[match_location+2:]
-        # token_count = self._counts[pre_token]
-        # self._counts.pop(pre_token)
-        # self._counts[updated_token] = token_count
+            updated_token = pre_token[:match_location] + (pre_token[match_location] + pre_token[match_location+1],) + pre_token[match_location+2:]
         return updated_token
 
 
 def tokenize(input_path: str, vocab_size, special_tokens=["<|endoftext|>"]):
     bpe_tokenizer = BPETokenizer(vocab_size, special_tokens)
     bpe_tokenizer.fit(input_path)
-    return bpe_tokenizer.vocab, bpe_tokenizer.merges
+    vocab = bpe_tokenizer.vocab
+    merges = bpe_tokenizer.merges
+    vocab_str = {str(k): v.decode('utf-8', 'replace') for k, v in vocab.items()}
+    with open('vocab.json', 'w') as f:
+        json.dump(vocab_str, f, ensure_ascii=False, indent=2)
+    
+    with open('merges.txt', 'w', encoding='utf-8') as f:
+        for a, b in merges:
+            f.write(f"{a.decode('utf-8', 'replace')} {b.decode('utf-8', 'replace')}\n")
+    return vocab, merges
 
 
 if __name__ == "__main__":
-    import cProfile
-    input_path = "/home/gulgul/Downloads/cs336/assignment1-basics/tests/fixtures/corpus.en"
-    cProfile.run('tokenize(input_path, 500)', 'profile_output_v1.prof')
+    # import cProfile
+    import time
+    input_path = "/home/gulgul/Downloads/cs336/assignment1-basics/data/TinyStoriesV2-GPT4-train.txt"
+    # cProfile.run('tokenize(input_path, 10000)', 'profile_output_v1.prof')
+    start_time = time.time()
+    tokenize(input_path, 10000)
+    print(f"Runtime: {time.time()-start_time}")
