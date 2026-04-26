7db03fc95278775c9ef958103794bb1939647adc
Abhinav Aggarwal <abhinavaggrwal@gmail.com>
2026-04-26 19:33:12 +0200

    Add self attention with rope

--- cs336_basics/models/attention.py
+++ cs336_basics/models/attention.py
@@ -1,6 +1,6 @@
 import torch
 from torch import Tensor, nn
-from jaxtyping import Float, Bool
+from jaxtyping import Float, Bool, Int
 from einops import einsum, rearrange
 
 from cs336_basics.models import softmax, Linear, RotaryPositionalEmbedding
@@ -30,16 +30,21 @@ class MultiHeadAttention(nn.Module):
         self.num_heads = num_heads
         self.l1 = Linear(d_model, 3*d_model, device=device, dtype=dtype)
         self.l2 = Linear(d_model, d_model, device=device, dtype=dtype)
+        self.rope = None
         if theta is not None and max_seq_len is not None:
-            self.rope = RotaryPositionalEmbedding(theta, d_model, max_seq_len, device)
+            self.rope = RotaryPositionalEmbedding(theta, d_model//num_heads, max_seq_len, device)
     
-    def forward(self, x: Float[Tensor, "batch_size seq_len d_model"]):
+    def forward(self, x: Float[Tensor, "batch_size seq_len d_model"], token_positions: Int[Tensor, " ... sequence_length"] | None = None):
         batch, seq_len, _ = x.shape
         x = self.l1(x)
         Qs, Ks, Vs = x.chunk(chunks=3, dim=-1)
         Qh = torch.stack(Qs.chunk(chunks=self.num_heads, dim=-1), dim=1)
         Kh = torch.stack(Ks.chunk(chunks=self.num_heads, dim=-1), dim=1)
         Vh = torch.stack(Vs.chunk(chunks=self.num_heads, dim=-1), dim=1)
+
+        if self.rope:
+            Qh = self.rope(Qh, token_positions)
+            Kh = self.rope(Kh, token_positions)
         mask = torch.triu(torch.ones((batch, self.num_heads, seq_len, seq_len))).transpose(-2, -1).to(bool)
         attention = scaled_dot_product_attention(Qh, Kh, Vh, mask)
         attention_concat = rearrange(attention, 'b h seq d_k -> b seq (h d_k)')
--- tests/adapters.py
+++ tests/adapters.py
@@ -202,7 +202,11 @@ def run_multihead_self_attention_with_rope(
         Float[Tensor, " ... sequence_length d_model"]: Tensor with the output of running your optimized, batched multi-headed attention
         implementation with the given QKV projection weights and input features.
     """
-    raise NotImplementedError
+    mha = MultiHeadAttention(d_model, num_heads, theta, max_seq_len)
+    weights = torch.concat((q_proj_weight, k_proj_weight, v_proj_weight), dim=0)
+    mha.l1.W.data = weights
+    mha.l2.W.data = o_proj_weight
+    return mha(in_features, token_positions)
 
 
 def run_rope(
